fb02cbc812b6bd9517c7c20747ec824bc387f817
Dandy Naufaldi <dandy.naufaldi@gmail.com>
2018-09-29 16:28:27 +0700

    add 3D N

--- main.js
+++ main.js
@@ -116,13 +116,7 @@ function triDiObj( vertices, verItemSize, verNumItem, colors, colItemSize, colNu
         // this.colorBuffer.numItems = this.colNumItem
     }
 }
-// -2.0, 3.0, 0.0,   //1
-//                     -2.0, 3.0, 0.0,     //2
-//                     -2.0, -3.0, 0.0,   //10
 
-//                     -2.0, 3.0, 0.0,   //2
-//                     -1.0, -3.0, 0.0,  //9
-//                     -2.0, -3.0, 0.0,   //10
 let triDiObjs = []
 //     -2  -1  0   1   2
 //-------------------------
@@ -143,115 +137,150 @@ let triDiObjs = []
 //-1.0, 1.0, 0.0,   //4
 //-1.0, -3.0, 0.0,  //1 
 //-2.0, -3.0, 0.0,   //2 
-let n = new triDiObj([-1.0, -3.0, 0.0,  //1 
-                    -2.0, -3.0, 0.0,   //2 
-                    -2.0, 3.0, 0.0,   //3
-                    -1.0, 1.0, 0.0,   //4
-                    -1.0, 3.0, 0.0,   //5
-                    1.0, -3.0, 0.0,   //6
-                    1.0, -1.0, 0.0,   //7
-                    2.0, -3.0, 0.0,   //8
-                    1.0, 3,0, 0.0,    //9
-                    2.0, 3.0, 0.0,    //10
-                    ],
-                    3, 3,
-                    [0.0, 0.5, 0.5, 1.0,
-                    0.0, 0.5, 0.5, 1.0,
-                    0.0, 0.5, 0.5, 1.0,
-                    0.0, 0.5, 0.5, 1.0,
-                    0.0, 0.5, 0.5, 1.0,
-                    0.0, 0.5, 0.5, 1.0,
-                    0.0, 0.5, 0.5, 1.0,
-                    0.0, 0.5, 0.5, 1.0,
-                    0.0, 0.5, 0.5, 1.0,
-                    0.0, 0.5, 0.5, 1.0,
-                    ],
-                    4, 3)
-
-let y = new triDiObj([1.0, 1.0, 0.0,
-                    -1.0, 1.0, 0.0,
-                    1.0, -1.0, 0.0,
-                    -1.0, -1.0, 0.0],
-                    3, 4,
-                    [1.0, 0.0, 0.0, 1.0,
-                    0.0, 1.0, 0.0, 1.0,
-                    0.0, 0.0, 1.0, 1.0],
-                    4, 4)
-console.log(n)        
-let triangleVertexPositionBuffer
-let triangleVertexColorBuffer
-let squareVertexPositionBuffer
-let squareVertexColorBuffer
+    
+let NVertexPositionBuffer
+let NVertexColorBuffer
+let NVertexIndexBuffer
 function initBuffers() {
-    // Triangle Position
-    triangleVertexPositionBuffer = gl.createBuffer()
-    gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexPositionBuffer)
+    // N Position
+    NVertexPositionBuffer = gl.createBuffer()
+    gl.bindBuffer(gl.ARRAY_BUFFER, NVertexPositionBuffer)
     let vertices = [
+        // front left
         -2.0, -3.0, 0.0,   //2 
         -1.0, -3.0, 0.0,  //1 
-        -2.0, 3.0, 0.0,   //3
         -1.0, 3.0, 0.0,   //5
+        -2.0, 3.0, 0.0,   //3
+        
+        // front diagonal
+        -1.0, 3.0, 0.0,   //5
+        -1.0, 1.0, 0.0,   //4
+        1.0, -3.0, 0.0,   //6
+        1.0, -1.0, 0.0,   //7
+
+        // front right
         1.0, -3.0, 0.0,   //6
         2.0, -3.0, 0.0,   //8
+        2.0, 3.0, 0.0,    //10
+        1.0, 3.0, 0.0,    //9
+
+        // back left
+        -2.0, -3.0, -1.0,   //2 
+        -1.0, -3.0, -1.0,  //1 
+        -1.0, 3.0, -1.0,   //5
+        -2.0, 3.0, -1.0,   //3
+        
+        // back diagonal
+        -1.0, 3.0, -1.0,   //5
+        -1.0, 1.0, -1.0,   //4
+        1.0, -3.0, -1.0,   //6
+        1.0, -1.0, -1.0,   //7
+
+        // back right
+        1.0, -3.0, -1.0,   //6
+        2.0, -3.0, -1.0,   //8
+        2.0, 3.0, -1.0,    //10
+        1.0, 3.0, -1.0,    //9
+
+        
+        // top diagonal
+        -1.0, 3.0, 0.0,   //5
+        1.0, -1.0, 0.0,   //7
+        1.0, -1.0, -1.0,   //7
+        -1.0, 3.0, -1.0,   //5
+
+        // vertical inner right
+        1.0, 3.0, 0.0,    //9
+        1.0, -1.0, 0.0,   //7
+        1.0, -1.0, -1.0,   //7
+        1.0, 3.0, -1.0,    //9
+
+        // vertical outer right
+        2.0, -3.0, 0.0,   //8
+        2.0, 3.0, 0.0,    //10
+        2.0, 3.0, -1.0,    //10
+        2.0, -3.0, -1.0,   //8
+
+        // bottom diagonal
+        -1.0, 1.0, 0.0,   //4
+        1.0, -3.0, 0.0,   //6
+        1.0, -3.0, -1.0,   //6
+        -1.0, 1.0, -1.0,   //4
+
+        // vertical inner left
+        -1.0, 1.0, 0.0,   //4
+        -1.0, -3.0, 0.0,  //1
+        -1.0, -3.0, -1.0,  //1
+        -1.0, 1.0, -1.0,   //4
+
+        // vertical outer left
+        -2.0, -3.0, 0.0,   //2
+        -2.0, 3.0, 0.0,   //3
+        -2.0, 3.0, -1.0,   //3
+        -2.0, -3.0, -1.0,   //2
+
+        // top left
+        -1.0, 3.0, 0.0,   //5
+        -2.0, 3.0, 0.0,   //3
+        -2.0, 3.0, -1.0,   //3
+        -1.0, 3.0, -1.0,   //5
+
+        // top right
         1.0, 3.0, 0.0,    //9
         2.0, 3.0, 0.0,    //10
+        2.0, 3.0, -1.0,    //10
+        1.0, 3.0, -1.0,    //9
+
+        // bottom right
+        2.0, -3.0, 0.0,   //8
+        1.0, -3.0, 0.0,   //6
+        1.0, -3.0, -1.0,   //6
+        2.0, -3.0, -1.0,   //8
+
+        // bottom left
+        -2.0, -3.0, 0.0,   //2
+        -1.0, -3.0, 0.0,  //1
+        -1.0, -3.0, -1.0,  //1
+        -2.0, -3.0, -1.0,   //2
     ]
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW)
-    triangleVertexPositionBuffer.itemSize = 3
-    triangleVertexPositionBuffer.numItems = 8
+    NVertexPositionBuffer.itemSize = 3
+    NVertexPositionBuffer.numItems = 64
     // Triangle Color
-    triangleVertexColorBuffer = gl.createBuffer()
-    gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexColorBuffer)
-    let colors = [
-        1.0, 0.0, 0.0, 1.0,
-        0.0, 1.0, 0.0, 1.0,
-        0.0, 0.0, 1.0, 1.0,
-        1.0, 0.0, 0.0, 1.0,
-        0.0, 1.0, 0.0, 1.0,
-        0.0, 0.0, 1.0, 1.0,
-        1.0, 0.0, 0.0, 1.0,
-        0.0, 1.0, 0.0, 1.0,
-    ]
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW)
-    triangleVertexColorBuffer.itemSize = 4
-    triangleVertexColorBuffer.numItems = 8
-    // Square Position
-    squareVertexPositionBuffer = gl.createBuffer()
-    gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexPositionBuffer)
-    vertices = [
-        1.0, -2.0, 0.0, //1
-        0.0, -2.0, 0.0, //2
-        1.0, 0.5, 0.0, //3
-        0.0, 0.5, 0.0, //4
-        -0.5, 3.5, 0.0, //5
-        -1.5, 3.0, 0.0, //6
-        1.0, 0.5, 0.0, //7
-        0.0, 0.5, 0.0, //8
-        2.5, 3.0, 0.0, //9
-        1.5, 3.5, 0.0 //10
-    ]
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW)
-    squareVertexPositionBuffer.itemSize = 3
-    squareVertexPositionBuffer.numItems = 10
-    // Square Color
-    squareVertexColorBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexColorBuffer);
-    colors = [
-        1.0, 0.0, 0.0, 1.0,
-        0.0, 1.0, 0.0, 1.0,
-        0.0, 0.0, 1.0, 1.0,
-        1.0, 0.0, 0.0, 1.0,
-        0.0, 1.0, 0.0, 1.0,
-        0.0, 0.0, 1.0, 1.0,
-        1.0, 0.0, 0.0, 1.0,
-        0.0, 1.0, 0.0, 1.0,
-        0.0, 0.0, 1.0, 1.0,
-        1.0, 0.0, 0.0, 1.0,
-    ]
+    NVertexColorBuffer = gl.createBuffer()
+    gl.bindBuffer(gl.ARRAY_BUFFER, NVertexColorBuffer)
+    let colors = []
     
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
-    squareVertexColorBuffer.itemSize = 4;
-    squareVertexColorBuffer.numItems = 10;
+    // front and back
+    for (let i = 0; i < 24; i++) {
+        colors = colors.concat([0.6, 0.6, 0.2, 1.0])
+    }
+
+    // inner and outer vertical, diagonal
+    for (let i = 0; i < 24; i++) {
+        colors = colors.concat([0.5, 0.5, 0.0, 1.0])
+    }
+
+    // top and bottom
+    for (let i = 0; i < 16; i++) {
+        colors = colors.concat([0.5, 0.5, 0.2, 1.0])
+    }
+    // console.log(colors)
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW)
+    NVertexColorBuffer.itemSize = 4
+    NVertexColorBuffer.numItems = 64
+    
+    NVertexIndexBuffer = gl.createBuffer()
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, NVertexIndexBuffer)
+    let NVertexIndices = []
+    for (let i = 0; i < 16; i++) {
+        NVertexIndices = NVertexIndices.concat([i*4, i*4 + 1, i*4 + 2])
+        NVertexIndices = NVertexIndices.concat([i*4, i*4 + 2, i*4 + 3])
+    }
+    // console.log(NVertexIndices)
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(NVertexIndices), gl.STATIC_DRAW)
+    NVertexIndexBuffer.itemSize = 1
+    NVertexIndexBuffer.numItems = 96
 }
 let rTri = 0
 let rSquare = 0
@@ -262,26 +291,30 @@ function drawScene() {
     mat4.identity(mvMatrix)
     mat4.translate(mvMatrix, mvMatrix, [-1.5, 3.0, -20.0])
     mvPushMatrix()
-    mat4.rotate(mvMatrix, mvMatrix, glMatrix.toRadian(rTri), [1.0, 0.0, 0.0])
+    mat4.rotate(mvMatrix, mvMatrix, glMatrix.toRadian(rTri), [0.0, 1.0, 0.0])
     //mat4.rotateY(mvMatrix, mvMatrix, glMatrix.toRadian(rTri))
-    gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexPositionBuffer)
-    gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, triangleVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0)
-    gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexColorBuffer)
-    gl.vertexAttribPointer(shaderProgram.vertexColorAttribute, triangleVertexColorBuffer.itemSize, gl.FLOAT, false, 0, 0)
+    gl.bindBuffer(gl.ARRAY_BUFFER, NVertexPositionBuffer)
+    gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, NVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0)
+    
+    gl.bindBuffer(gl.ARRAY_BUFFER, NVertexColorBuffer)
+    gl.vertexAttribPointer(shaderProgram.vertexColorAttribute, NVertexColorBuffer.itemSize, gl.FLOAT, false, 0, 0)
+    
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, NVertexIndexBuffer)
+
     setMatrixUniforms()
-    gl.drawArrays(gl.TRIANGLE_STRIP, 0, triangleVertexPositionBuffer.numItems)
+    gl.drawElements(gl.TRIANGLES, NVertexIndexBuffer.numItems, gl.UNSIGNED_SHORT, 0)
     mvPopMatrix()
-    mat4.translate(mvMatrix, mvMatrix, [0.0, -7.0, 0.0])
-    mvPushMatrix()
-    mat4.rotate(mvMatrix, mvMatrix, glMatrix.toRadian(rSquare), [0.0, 1.0, 0.0])
-    //mat4.rotateX(mvMatrix, mvMatrix, glMatrix.toRadian(rSquare))
-    gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexPositionBuffer)
-    gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, squareVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0)
-    gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexColorBuffer)
-    gl.vertexAttribPointer(shaderProgram.vertexColorAttribute, squareVertexColorBuffer.itemSize, gl.FLOAT, false, 0, 0)
-    setMatrixUniforms()
-    gl.drawArrays(gl.TRIANGLE_STRIP, 0, squareVertexPositionBuffer.numItems)
-    mvPopMatrix()
+    // mat4.translate(mvMatrix, mvMatrix, [0.0, -7.0, 0.0])
+    // mvPushMatrix()
+    // mat4.rotate(mvMatrix, mvMatrix, glMatrix.toRadian(rSquare), [0.0, 1.0, 0.0])
+    // //mat4.rotateX(mvMatrix, mvMatrix, glMatrix.toRadian(rSquare))
+    // gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexPositionBuffer)
+    // gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, squareVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0)
+    // gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexColorBuffer)
+    // gl.vertexAttribPointer(shaderProgram.vertexColorAttribute, squareVertexColorBuffer.itemSize, gl.FLOAT, false, 0, 0)
+    // setMatrixUniforms()
+    // gl.drawArrays(gl.TRIANGLE_STRIP, 0, squareVertexPositionBuffer.numItems)
+    // mvPopMatrix()
 }
 let lastTime = 0
 function animate() {
